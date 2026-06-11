628c5adedeabd82fddb28b61a33c6ec02b6d507a
pc <test>
2026-06-11 02:41:19 -0400

    url

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,10 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: './prisma/schema.prisma',
   datasource: {
-    url: env("DATABASE_URL"),
-  },
-  migrations: {
-    seed: 'npx tsx prisma/seed.ts',
+    // ✅ Aquí es donde Prisma 7 ahora busca tu conexión
+    url: env('DATABASE_URL'),
   },
 });
